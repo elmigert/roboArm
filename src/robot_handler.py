5e293953c5dc49c1d631428cfe22c4b3ab97b025
alr <alr@fp-robotics.com>
2019-12-03 13:53:33 +0100

    added position new function

--- src/robot_handler.py
+++ src/robot_handler.py
@@ -5,6 +5,8 @@ This file contains the RobotHandler class, which handles the communication with 
 """
 
 import time
+
+from src.geometry_helper import GeometryHelper
 from uarm_python_sdk.uarm.wrapper import SwiftAPI
 
 
@@ -20,11 +22,27 @@ class RobotHandler:
         self.__swift.set_mode(0)
         # reset arm to home
         self.__swift.reset(wait=True, speed=10000)
+        # initialize geometry helper
+        self.__geometry_helper = GeometryHelper()
 
     def disconnect(self):
         """
-        disconnect robot
+        Disconnect robot.
         """
         self.__swift.flush_cmd()
         time.sleep(3)
         self.__swift.disconnect()
+
+    def position_new(self, x_user, y_user):
+        """
+        Move robot arm to new position x, y in user frame.
+        :param x_user: new x position in user frame.
+        :type x_user: int
+        :param y_user: new y position in user frame.
+        :type y_user: int
+        """
+        # transform frames of positions
+        uarm_dict = self.__geometry_helper.transform_position_user_to_uarm(x_user, y_user)
+        # move arm
+        self.__swift.set_position(uarm_dict['x'], uarm_dict['y'])
+        self.__swift.flush_cmd()
